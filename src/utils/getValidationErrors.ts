9ec29b7088408838efd6c719c5db11dbc5aa4180
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-07-11 10:52:08 -0300

    Utils

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,15 @@
+import { ValidationError } from 'yup'
+
+interface Errors {
+  [key: string]: string
+}
+
+export default function getValidationErrors(error: ValidationError) : Errors {
+  const validationErrors: Errors = error.inner.reduce(
+    (result: Errors, error) => {
+      result[error.path] = error.message;
+      return result
+    }, {})
+
+  return validationErrors
+}
