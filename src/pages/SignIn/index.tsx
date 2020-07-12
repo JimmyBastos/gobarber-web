41ff9c63c9b417f0a42739156d99ead58323e436
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-07-12 19:47:48 -0300

    SignIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useCallback, useContext } from 'react'
+import React, { useRef, useCallback } from 'react'
+import { Link } from 'react-router-dom'
 import { Form } from '@unform/web'
 import { FormHandles } from '@unform/core'
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi'
-import { Container, Content, Background } from './styles'
+import { Container, Transition, Content, Background } from './styles'
 
 import * as Yup from 'yup'
 
@@ -12,14 +13,21 @@ import Button from '../../components/Button'
 import Input from '../../components/Input'
 import getValidationErrors from '../../utils/getValidationErrors'
 
-import { AuthContext } from '../../context/AuthContext'
+import { useAuth } from '../../hooks/auth'
+import { useToast } from '../../hooks/toast'
+
+interface SignInFormData {
+  email: string,
+  password: string
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { signIn } = useContext(AuthContext)
+  const { signIn } = useAuth()
+  const { addToast } = useToast()
 
-  const handleLogin = useCallback(async (data: object) => {
+  const handleLogin = useCallback(async (formData: SignInFormData) => {
     try {
       formRef.current?.setErrors({})
 
@@ -31,60 +39,69 @@ const SignIn: React.FC = () => {
           .required('Senha obrigatória')
       })
 
-      await schema.validate(data, { abortEarly: false })
+      await schema.validate(formData, { abortEarly: false })
 
-      signIn(data)
+      await signIn(formData)
     } catch (error) {
-      formRef.current?.setErrors(
-        getValidationErrors(error)
-      )
+      if (error instanceof Yup.ValidationError) {
+        formRef.current?.setErrors(
+          getValidationErrors(error)
+        )
+      } else {
+        console.error(error)
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação!',
+          description: 'Não foi possível fazer login. Verifique seu e-mail e senha.'
+        })
+      }
     }
-  }, [signIn])
+  }, [signIn, addToast])
 
   return (
     <Container>
-      <Content>
-        <img src={logoImage} alt="GoBarber" />
+      <Transition>
+        <Content>
+          <img src={logoImage} alt="GoBarber" />
 
-        <Form ref={formRef} className="login-form" onSubmit={handleLogin}>
-          <h1 className="login-form__title">Faça seu Login</h1>
+          <Form ref={formRef} className="login-form" onSubmit={handleLogin}>
+            <h1 className="login-form__title">Faça seu Login</h1>
 
-          <Input
-            name="email"
-            type="email"
-            icon={FiMail}
-            placeholder="E-mail"
-          />
+            <Input
+              name="email"
+              type="email"
+              icon={FiMail}
+              placeholder="E-mail"
+            />
 
-          <Input
-            name="password"
-            type="password"
-            icon={FiLock}
-            placeholder="Senha"
-          />
+            <Input
+              name="password"
+              type="password"
+              icon={FiLock}
+              placeholder="Senha"
+            />
 
-          <Button
-            type="submit"
-          >
+            <Button
+              type="submit"
+            >
             Entrar
-          </Button>
+            </Button>
 
-          <a
-            className="login-form__recover-pass"
-            href="#forgot"
-          >
+            <a
+              className="login-form__recover-pass"
+              href="#forgot"
+            >
             Esqueci minha senha
-          </a>
-        </Form>
+            </a>
+          </Form>
 
-        <a className="create-account" href="#cadastro">
-          <FiLogIn />
+          <Link className="create-account" to="/signup">
+            <FiLogIn />
             Criar Uma Conta
-        </a>
-      </Content>
-      <Background>
-
-      </Background>
+          </Link>
+        </Content>
+      </Transition>
+      <Background/>
     </Container>
   )
 }
