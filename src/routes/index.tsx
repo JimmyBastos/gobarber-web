9e7f62f5874500f2ac23c633bc9e6da6c7d78820
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-11-28 21:47:15 -0300

    Add profile and passoword routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,19 @@ import Route from './Route'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 import Dashboard from '../pages/Dashboard'
+import Profile from '../pages/Profile'
+import ForgotPassword from '../pages/ForgotPassword'
+import ResetPassword from '../pages/ResetPassword'
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" component={SignIn} exact/>
       <Route path="/signup" component={SignUp} />
+      <Route path="/forgot-password" component={ForgotPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   )
 }
